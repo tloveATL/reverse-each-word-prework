0e179fe139c85c463586b84e99b718141ede2d31
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-14 00:35:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,4 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split
-  sentence_array.collect { |word| word.reverse}
-  sentence_array.join
+  sentence_array.collect { |word| (word.reverse).join}
 end
